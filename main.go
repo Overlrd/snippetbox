70061334cfac6c972b4b691fd85cee6125a79024
augustin <premieraugust28@gmail.com>
2025-12-31 10:21:46 +0000

    Method based routing: GET

--- main.go
+++ main.go
@@ -44,9 +44,9 @@ func snippetCreate(w http.ResponseWriter, r *http.Request)  {
 func main() {
 	// initialize new servermux
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
 
 	// Start a new server with http.ListenAndServe
 	log.Println("Starting server on: 4000")
