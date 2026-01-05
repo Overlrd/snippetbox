c41a060faede7741a9e860c766ee305cc57c8d2c
augustin <premieraugust28@gmail.com>
2026-01-05 17:03:53 +0000

    Isolating tha application routes
    Moved the routes declarations for the application into a standalone
    routes.go file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -29,31 +29,10 @@ func main() {
 		logger:logger,
 	}
 
-	// initialize new servermux
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir functin is relative to the project directory root.
-	// We're using a custom FileSystem that checks if the requested path is a directory
-	// If it is a directory we then try to Open() any index.html file in it. If no index.html
-	// file exists, then this will return a os.ErrNotExist error (which in turn we return and
-	// it will be transformed into a 404 Not Found response by http.Fileserver).
-	fileserver := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-	mux.Handle("./ui/static", http.NotFoundHandler())
-
-	// Use the mux.Handler() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static/" prefix before the request reaches the file server
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.getSnippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.postSnippetCreate)
+	logger.Info("starting server", "addr", *addr)
 
 	// Start a new server with http.ListenAndServe
-	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
