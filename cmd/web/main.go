0d0c146539eefe746cf07154b87e4e21d22b2a91
augustin <premieraugust28@gmail.com>
2026-01-06 08:35:13 +0000

    Creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,6 +18,7 @@ type application struct {
 func main() {
 	// Define command line flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Initialize a new logger
@@ -22,6 +26,17 @@ func main() {
 		Level: slog.LevelDebug,
 		AddSource: true,
 	}))
+
+	// Creating a connection pool to the database
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// Defer a call to db.Close(), sp that the connection pool is closed
+	// before the main() function exists.
+	defer db.Close()
 
 	// Initialize a new instance of the application struct, containing the
 	// dependencies
@@ -32,9 +47,25 @@ func main() {
 	logger.Info("starting server", "addr", *addr)
 
 	// Start a new server with http.ListenAndServe
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// The openDB functon wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
 
 type neuteredFileSystem struct {
