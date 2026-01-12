2ed3e3c2d31137b610c8f393f8f482d3f9ef43fd
augustin <premieraugust28@gmail.com>
2026-01-12 10:29:12 +0000

    Working with session data
    feat session management and cookies
    feat autodisplaying flash messages to user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -137,5 +137,8 @@ func (app *application) postSnippetCreate(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Add flash message
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
+
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
